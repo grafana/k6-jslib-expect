ec7b3397fa903a78e7ad75034f01a01ab71e5aa5
Pawel Suwala <pawel.suwala@gmail.com>
2021-10-16 00:11:43 +0200

    Drop the legacy assertion functions
    Going full chai

--- src/expect.js
+++ src/expect.js
@@ -3,6 +3,36 @@ import Ajv from 'https://jslib.k6.io/ajv/6.12.5/index.js';
 
 import { expect, util, Assertion, AssertionError, config } from "https://cdnjs.cloudflare.com/ajax/libs/chai/4.3.4/chai.min.js";
 
+const truncateString = (str, len) => str.length > len ? `${str.substring(0, len)}...` : str;
+
+config.truncateValueThreshold = 100; // individual variables should be up to 100 chars.
+config.truncateMsgThreshold = 300; // whole check() message must be below 300 chars.
+
+function k6getMessage(obj, args){
+  var negate = util.flag(obj, 'negate')
+    , val = util.flag(obj, 'object')
+    , expected = args[3]
+    , actual = util.getActual(obj, args)
+    , msg = negate ? args[2] : args[1]
+    , flagMsg = util.flag(obj, 'message')
+    , anonymizeMsgFunction = util.flag(obj, 'anonymizeMsgFunction');
+
+  if(anonymizeMsgFunction){
+    msg = anonymizeMsgFunction(msg)
+  }
+
+  if(typeof msg === "function") msg = msg();
+  msg = msg || '';
+  msg = msg
+    .replace(/#\{this\}/g, function () { return truncateString(util.objDisplay(val), config.truncateValueThreshold) })
+    .replace(/#\{act\}/g, function () { return truncateString(util.objDisplay(actual), config.truncateValueThreshold) })
+    .replace(/#\{exp\}/g, function () { return truncateString(util.objDisplay(expected), config.truncateValueThreshold) });
+config.truncateValueThreshold
+  msg = flagMsg ? flagMsg + ': ' + msg : msg;
+
+  return truncateString(msg, config.truncateMsgThreshold);
+}
+
 /*
 Hacking the Chai library to use k6 checks.
 */
@@ -13,7 +43,8 @@ util.overwriteMethod(Assertion.prototype, 'assert', function (_super) {
     if (undefined === expected && undefined === _actual) showDiff = false;
     if (true !== config.showDiff) showDiff = false;
 
-    msg = util.getMessage(this, arguments);
+    msg = k6getMessage(this, arguments);
+
     var actual = util.getActual(this, arguments);
     var assertionErrorObjectProperties = {
         actual: actual
@@ -27,8 +58,6 @@ util.overwriteMethod(Assertion.prototype, 'assert', function (_super) {
     }
 
     if (!ok) {
-
-      // console.warn(msg)
 
       check(null, {
         [msg]: false
@@ -47,254 +76,68 @@ util.overwriteMethod(Assertion.prototype, 'assert', function (_super) {
 });
 
 
-export class FunkBrokenChainException extends Error {
-  constructor(message) {
-    super(message);
-    this.brokenChain = true;
-    this.name = this.constructor.name;
-    if (typeof Error.captureStackTrace === 'function') {
-      Error.captureStackTrace(this, this.constructor)
-    } else {
-      this.stack = new Error(message).stack
-    }
-  }
-}
 
-class Funk {
-  constructor() {
-    this.leftHandValue = null;     // resp.status
-    this.leftHandValueName = null; // "my status"
-    this.rightHandValue = null;    // 200
-    this.chainBroken = false;
-    this.printedBrokenChainWarning = false; // print only one warning.
-    this.ajv = new Ajv();
-  };
+const _ajv = new Ajv();
 
-  as(name) {
-    this.leftHandValueName = name;
-    return this
-  }
+util.addMethod(Assertion.prototype, 'matchApiSchema', function (schema) {
+  var data = util.flag(this, 'object');
+  // var data = util.flag(this, 'apiSchemaMessage', "Hejo");
 
-  _brokenChainCheck() {
-    if (this.chainBroken) {
-      if (!this.printedBrokenChainWarning) {
-        console.warn("This check has been aborted because the previous check in the chain has failed");
-        this.printedBrokenChainWarning = true;
-      }
-      return true
-    }
-    return false;
-  }
+  // new chai.Assertion(obj).to.be.equal(str);
+  let validate = _ajv.compile(schema);
 
-  _recordCheck(checkName, isSuccessful, value) {
-    if (value !== undefined) {
-      check(null, {
-        [checkName]: isSuccessful
-      }, {
-        value: value
+  let is_valid = validate(data, schema);
+
+  // optional. It records specific error messages as checks.
+  if (!is_valid) {
+    console.error(JSON.stringify(validate.errors));
+
+    validate.errors.forEach(error => {
+      check(is_valid, {
+        [`XXX ${error.dataPath} ${error.message}`]: (is_valid) => is_valid
       });
-    }
-    else {
-      check(null, {
-        [checkName]: isSuccessful
-      });
-    }
-  }
-
-  _breakTheChain() {
-    this.chainBroken = true;
-    throw new FunkBrokenChainException("Chain broke, skipping this check");
-  }
-
-  _leftHandValueIsHttpResponse(calee) {
-    // TODO: I don't know how to check that this.leftHandValue is of type HttpResponse 
-    if (this.leftHandValue && this.leftHandValue.hasOwnProperty('request')) {
-      return true;
-    }
-    else {
-      console.error(`The object passed to expect/and for ${calee} isn't a k6 HttpResponse. Aborting the check.`);
-      this._breakTheChain();
-      this.printedBrokenChainWarning = true;
-      return false
-    }
-  }
-
-  toMatchAPISchema(schema) {
-    let validate = this.ajv.compile(schema);
-    let data = this.leftHandValue;
-
-    let is_valid = validate(data, schema);
-    let name = this.leftHandValueName || '';
-
-    check(is_valid, {
-      [`${name} schema validation`]: (is_valid) => is_valid
     });
-
-    // optional. It records specific error messages as checks.
-    if (!is_valid) {
-      // console.error(JSON.stringify(validate.errors));
-
-      validate.errors.forEach(error => {
-        check(is_valid, {
-          [`${name} ${error.dataPath} ${error.message}`]: (is_valid) => is_valid
-        });
-      });
-      this._breakTheChain();
-    }
-
-    return this;
   }
 
-  toHaveValidJson() {
-    if (this._brokenChainCheck()) return this;
-    if (!this._leftHandValueIsHttpResponse("toHaveValidJson")) return this;
+  this.assert(
+    is_valid
+  , "expected to match API schema"
+  , "expected to not not match the API schema"
+  , null   // expected
+  , null   // actual
+  );  
+});
 
-    let resp = this.leftHandValue;
-
-    let checkIsSuccessful = true;
-    try {
-      resp.json();
-    }
-    catch (e) {
-      checkIsSuccessful = false;
-    }
-
-    let checkName = `${resp.request.url} has valid json response`
-    this._recordCheck(checkName, checkIsSuccessful);
-    if (!checkIsSuccessful) this._breakTheChain();
-
-    return this
+util.addMethod(Assertion.prototype, 'anonymize', function (anonymizeMsgFunction) {
+  anonymizeMsgFunction = anonymizeMsgFunction || function(msg) {
+    return msg.replace(/#\{this\}/g, function () { return "<anonymized>"; })
   }
 
-  toEqual(rhv) {
-    if (this._brokenChainCheck()) return this;
-    this.rightHandValue = rhv;
+  util.flag(this, 'anonymizeMsgFunction', anonymizeMsgFunction);
+});
 
-    let checkName = `${this.leftHandValue} is ${this.rightHandValue}`;
 
-    let checkIsSuccessful = this.leftHandValue === this.rightHandValue;
+util.addMethod(Assertion.prototype, 'validJsonBody', function () {
 
-    if (this.leftHandValueName) {
-      checkName = `${this.leftHandValueName} is ${this.leftHandValue}.`;
+  var response = util.flag(this, 'object');
 
-      if (!checkIsSuccessful) {
-        checkName += ` Expected '${this.rightHandValue}'`;
-      }
-    }
-
-    this._recordCheck(checkName, checkIsSuccessful, this.rightHandValue);
-
-    if (!checkIsSuccessful) this._breakTheChain();
-
-    return this;
+  let checkIsSuccessful = true;
+  try {
+    response.json();
+  }
+  catch (e) {
+    checkIsSuccessful = false;
   }
 
-  toBeGreaterThan(rhv) {
-    if (this._brokenChainCheck()) return this;
+  this.assert(
+    checkIsSuccessful
+  , `has valid json body`
+  , "does not have a valid json body"
+  , null   // expected
+  , null   // actual
+  );    
 
-    this.rightHandValue = rhv;
-
-    let checkName = `${this.leftHandValueName || this.leftHandValue} is greater than ${this.rightHandValue}`;
-
-    let checkIsSuccessful = this.leftHandValue > this.rightHandValue;
-
-    this._recordCheck(checkName, checkIsSuccessful, this.leftHandValue);
-
-    if (!checkIsSuccessful) this._breakTheChain();
-
-    return this;
-  }
-
-  toBeGreaterThanOrEqual(rhv) {
-    if (this._brokenChainCheck()) return this;
-
-    this.rightHandValue = rhv;
-
-    let checkName = `${this.leftHandValueName || this.leftHandValue} is greater or equal to ${this.rightHandValue}`;
-
-    let checkIsSuccessful = this.leftHandValue >= this.rightHandValue;
-
-    this._recordCheck(checkName, checkIsSuccessful, this.leftHandValue);
-
-    if (!checkIsSuccessful) this._breakTheChain();
-
-    return this;
-  }
-  toBeLessThan(rhv) {
-    if (this._brokenChainCheck()) return this;
-
-    this.rightHandValue = rhv;
-
-    let checkName = `${this.leftHandValueName || this.leftHandValue} is less than ${this.rightHandValue}`;
-
-    let checkIsSuccessful = this.leftHandValue < this.rightHandValue;
-
-    this._recordCheck(checkName, checkIsSuccessful, this.leftHandValue);
-
-    if (!checkIsSuccessful) this._breakTheChain();
-
-    return this;
-  }
-  toBeLessThanOrEqual(rhv) {
-    if (this._brokenChainCheck()) return this;
-
-    this.rightHandValue = rhv;
-
-    let checkName = `${this.leftHandValueName || this.leftHandValue} is less or equal to ${this.rightHandValue}`;
-
-    let checkIsSuccessful = this.leftHandValue <= this.rightHandValue;
-
-    this._recordCheck(checkName, checkIsSuccessful, this.leftHandValue);
-
-    if (!checkIsSuccessful) this._breakTheChain();
-
-    return this;
-  }
-
-  toBeTruthy() {
-    if (this._brokenChainCheck()) return this;
-
-    let checkName = `${this.leftHandValueName || this.leftHandValue} is truthy.`;
-
-    let checkIsSuccessful = this.leftHandValue ? true : false;
-
-    this._recordCheck(checkName, checkIsSuccessful, this.leftHandValue);
-
-    if (!checkIsSuccessful) this._breakTheChain();
-
-    return this;
-  }
-
-  toBeBetween(from, to) {
-    if (this._brokenChainCheck()) return this;
-
-    this.rightHandValue = `${from} - ${to}`;
-
-    let checkName = `${this.leftHandValueName || this.leftHandValue} is between ${this.rightHandValue}`;
-
-    let checkIsSuccessful = this.leftHandValue >= from && this.leftHandValue <= to;
-
-    this._recordCheck(checkName, checkIsSuccessful, this.leftHandValue);
-
-    if (!checkIsSuccessful) this._breakTheChain();
-
-    return this;
-  }
-
-  and(lhv) { // same as expect() but chained.
-    if (this._brokenChainCheck()) return this;
-    this.leftHandValue = lhv;
-    this.leftHandValueName = null; // clearing the previous .as()
-    return this;
-  }
-
-}
-
-let ourLegacyExpect = function (value1) {
-  let state = new Funk();
-  state.leftHandValue = value1;
-  return state;
-};
+});
 
 function handleUnexpectedException(e, testName) {
   console.error(`Exception raised in test "${testName}". Failing the test and continuing. \n${e}`)
@@ -352,15 +195,11 @@ function handleUnexpectedException(e, testName) {
 
 let describe = function (stepName, stepFunction) {
 
-  let t = {
-    'expect': ourLegacyExpect
-  };
-
   let success = true;
 
   group(stepName, () => {
     try {
-      stepFunction(t);
+      stepFunction();
       success = true;
     }
     catch (e) {
